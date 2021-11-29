24bf95d89a811ad82289441355fdc19ca8d3c6b0
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-11-29 22:35:41 +0000

    Add task running multiqc for bam files (#14)
    * add task running multiqc for bam files
    
    * add task to target
    
    * add files to final checklist
    
    * fix expected filenames
    
    * fix leftover

--- etc/ci_checks.sh
+++ etc/ci_checks.sh
@@ -7,10 +7,10 @@ expected_files=(
     results/qc/fastqc/sample_01_2_fastqc.zip
     results/qc/fastqc/sample_02_1_fastqc.zip
     results/qc/fastqc/sample_02_2_fastqc.zip
-    results/reports/multiqc/fastqc.html
-    results/reports/multiqc/fastqc_data/multiqc_fastqc.txt
-    results/reports/multiqc/fastqc_data/multiqc_general_stats.txt
-    results/reports/multiqc/fastqc_data/multiqc_sources.txt
+    results/reports/multiqc/fastq.html
+    results/reports/multiqc/fastq_data/multiqc_general_stats.txt
+    results/reports/multiqc/fastq_data/multiqc_sources.txt
+    results/reports/multiqc/fastq_data/multiqc_fastqc.txt
     results/hisat2/sample_01.bam
     results/hisat2/sample_02.bam
     results/hisat2/sample_01.bam.bai
@@ -23,6 +23,13 @@ expected_files=(
     results/qc/picard/CollectAlignmentSummaryMetrics/sample_02
     results/qc/picard/CollectInsertSizeMetrics/sample_01
     results/qc/picard/CollectInsertSizeMetrics/sample_02
+    results/reports/multiqc/bam.html
+    results/reports/multiqc/bam_data/multiqc_general_stats.txt
+    results/reports/multiqc/bam_data/multiqc_sources.txt
+    results/reports/multiqc/bam_data/multiqc_bowtie2.txt
+    results/reports/multiqc/bam_data/multiqc_picard_AlignmentSummaryMetrics.txt
+    results/reports/multiqc/bam_data/multiqc_picard_insertSize.txt
+    results/reports/multiqc/bam_data/multiqc_samtools_flagstat.txt
 )
 
 for file in "${expected_files[@]}"
