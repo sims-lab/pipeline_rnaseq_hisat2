d743e10653702c7639e33088a9450909ce474451
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-11-29 09:16:06 +0000

    add files to ci check list

--- etc/ci_checks.sh
+++ etc/ci_checks.sh
@@ -3,6 +3,14 @@ expected_files=(
     results/fastqc/sample_01_2_fastqc.html
     results/fastqc/sample_01_1_fastqc.html
     results/fastqc/sample_01_2_fastqc.html
+    results/fastqc/sample_01_1_fastqc.zip
+    results/fastqc/sample_01_2_fastqc.zip
+    results/fastqc/sample_02_1_fastqc.zip
+    results/fastqc/sample_02_2_fastqc.zip
+    results/multiqc/fastqc.html
+    results/multiqc/fastqc_data/multiqc_fastqc.txt
+    results/multiqc/fastqc_data/multiqc_general_stats.txt
+    results/multiqc/fastqc_data/multiqc_sources.txt
 )
 
 for file in "${expected_files[@]}"
