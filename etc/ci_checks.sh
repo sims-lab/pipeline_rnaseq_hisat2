c1ca44869382a99e5924eb29ea576f7173bed2ac
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-11-29 22:23:54 +0000

    fix expected filenames

--- etc/ci_checks.sh
+++ etc/ci_checks.sh
@@ -8,9 +8,9 @@ expected_files=(
     results/qc/fastqc/sample_02_1_fastqc.zip
     results/qc/fastqc/sample_02_2_fastqc.zip
     results/reports/multiqc/fastq.html
-    results/reports/multiqc/fastq_data/multiqc_fastqc.txt
     results/reports/multiqc/fastq_data/multiqc_general_stats.txt
     results/reports/multiqc/fastq_data/multiqc_sources.txt
+    results/reports/multiqc/fastq_data/multiqc_fastqc.txt
     results/hisat2/sample_01.bam
     results/hisat2/sample_02.bam
     results/hisat2/sample_01.bam.bai
@@ -24,9 +24,13 @@ expected_files=(
     results/qc/picard/CollectInsertSizeMetrics/sample_01
     results/qc/picard/CollectInsertSizeMetrics/sample_02
     results/reports/multiqc/bam.html
-    results/reports/multiqc/bam_data/multiqc_fastqc.txt
     results/reports/multiqc/bam_data/multiqc_general_stats.txt
     results/reports/multiqc/bam_data/multiqc_sources.txt
+    results/reports/multiqc/bam_data/multiqc_bowtie2.txt
+    results/reports/multiqc/bam_data/multiqc_picard_AlignmentSummaryMetrics.txt
+    results/reports/multiqc/bam_data/multiqc_picard_insertSize.txt
+    results/reports/multiqc/bam_data/multiqc_samtools_flagstat.txt
+    results/reports/multiqc/bam_data/
 )
 
 for file in "${expected_files[@]}"
