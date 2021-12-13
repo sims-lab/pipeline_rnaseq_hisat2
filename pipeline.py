11e649e17cf74d17ca0e932ec02094a8e7435b66
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-12-13 17:48:57 +0000

    remove debug messages

--- pipeline.py
+++ pipeline.py
@@ -70,12 +70,14 @@ from cgatcore import pipeline as P
 #################
 # Configuration #
 #################
-print("%s/config/pipeline.yml" % os.path.dirname(os.path.realpath(__file__)))
+
+
 # Load parameters from config file, located in `./config/pipeline.yml`.
 PARAMS = P.get_parameters(
     "%s/config/pipeline.yml" % os.path.dirname(os.path.realpath(__file__))
 )
-print(PARAMS)
+
+
 ############
 # Workflow #
 ############
